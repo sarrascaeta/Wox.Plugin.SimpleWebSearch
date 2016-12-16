5c454484771e9147afb1f0bb70d4ac4c153f5591
Sergio Arrascaeta <sergio7@gmail.com>
2016-12-16 10:03:23 -0700

    Initial commit

---
+++ SimpleWebSearch/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wox.Plugin;
+
+namespace SimpleWebSearch
+{
+    class Main : IPlugin
+    {
+        private PluginInitContext context;
+
+        public void Init(PluginInitContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+            results.Add(new Result()
+            {
+                Title = $"{query}",
+                SubTitle = $"Search Google for '{query}'",
+                IcoPath = "Images\\google.png",  //relative path to your plugin directory
+                Score = 5,
+                Action = e =>
+                {
+                    Process.Start("http://google.com/search?q=" + query);
+                    return true;
+                }
+            });
+
+            return results;
+        }
+
+    }
+}
